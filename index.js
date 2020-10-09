ebdfd775d326374b7eb4cdf301b6c63130138cb0
Sean Kramer <seandominickramer@gmail.com>
2020-10-09 18:32:36 -0400

    Add logger middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const projectRouter = require('./data/helpers/projectRouter');
+const logger = require('./middleware/logger');
 
 
 const server = express();
 const port = 8080;
 
 server.use(express.json());
+
+server.use(logger);
 
 
 server.get('/', (req, res) => {
